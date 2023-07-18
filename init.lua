f8ae1c8f813c29dd8fabced7db534850a3cbbd0c
autonomuscoder <mahiabirgit@tutanota.com>
2023-07-18 13:25:02 +0600

    sync systemc clipboard

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require ("core.packer")
 require ("catppuccin")
 require ("core.keys")
 require ("core.completion")
+require ("core.clipboard")
 require("mason").setup()
 require("mason-lspconfig").setup {
     ensure_installed = { "lua_ls", "rust_analyzer" },
